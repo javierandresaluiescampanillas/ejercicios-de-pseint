de3799d44a41d2342949eff80fc3be5d5068bdba
Javier <72435329+javierandresaluiescampanillas@users.noreply.github.com>
2020-10-13 20:38:04 +0200

    Add files via upload
    The first 4 java exercises.

---
+++ Java/Ejercicio2.java
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 2 de la relación 1
+ *
+ * @author Javier Andrés Domínguez
+ */
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    System.out.println("Javier Andrés Domínguez");
+	System.out.println("663663367");
+  }
+}
